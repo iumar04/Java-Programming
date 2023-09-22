ba104565e31322e7ce4b80254111b975d0cc69df
iumar04 <umarsangam9026@hotmail.com>
2023-09-23 00:54:42 +0530

    Java

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class test {
+	public static void main(String[] args){
+	
+		Scanner sc=new Scanner(System.in);
+		sc.useRadix(8);
+		int x=sc.nextInt();
+		System.out.println(x);
+	}		
+}
