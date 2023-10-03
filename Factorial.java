760e4204b746819b3b4a4c2541f4aef07fb92252
iumar04 <umarsangam9026@hotmail.com>
2023-10-03 19:40:12 +0530

    Java

--- Factorial.java
+++ Factorial.java
@@ -5,11 +5,17 @@ public class Factorial {
         int n;
         long fact=1;
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the Number : ");
+        System.out.println("Enter the Number (out of 20): ");
         n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            fact=fact*i;
+        if (n==0){
+            System.out.println("Factorial of the Number: 1");
         }
-        System.out.println("Factorial of the Number : "+fact);
+        else {
+            for (int i = 1; i <= n; i++) {
+                fact = fact * i;
+            }
+
+        System.out.println("Factorial of the Number: "+fact);
+        }
     }
 }
